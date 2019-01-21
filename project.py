327cb759314029a010534cb0a1441c00e0c0187c
Pranav Raghavan <gtalkpranav@gmail.com>
2019-01-20 18:56:40 -0800

    oauth_2 not working

--- project.py
+++ project.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
-app = Flask(__name__)
 from flask import session as login_session
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
@@ -9,6 +8,14 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
 import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Restaurant Menu"
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
